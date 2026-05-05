62062e0d2be062100576bf276ae849606e105eb0
Gabriel <gabriel.fontanari@owntec.com.br>
2026-05-05 10:47:48 -0300

    correções de organização e bugs

---
+++ src/historySection.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const history = ref(JSON.parse(localStorage.getItem('dataBase')) || []);
+</script>
+
+<template>
+  <div class="history-container">
+    <h1>Historico</h1>
+
+    <button @click="router.push('/')">
+      Voltar
+    </button>
+
+    <ul>
+      <li v-for="usr in history" :key="usr.name">
+        {{ usr.name }} - Altura: {{ usr.height }} - Peso: {{ usr.weight }} - IMC: {{ usr.imc }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.history-container {
+  padding: 2rem;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
